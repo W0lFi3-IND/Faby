be8716260e0e3c63bc1f40eec43cf80a48078735
Yashovardhan Shaktawat <yashovardhanshaktawat@gmail.com>
2020-10-04 12:34:28 +0530

    Main setup completed

---
+++ app/src/main/java/com/findingbetteryou/faby/OnBoarding/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.findingbetteryou.faby.OnBoarding;
+
+import android.os.Bundle;
+
+import com.findingbetteryou.faby.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
