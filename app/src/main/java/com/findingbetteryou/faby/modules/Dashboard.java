b562e3563b44fb1c74cac33a5517828e69c20179
Yashovardhan Shaktawat <yashovardhanshaktawat@gmail.com>
2020-10-24 11:16:39 +0530

    OnBoarding

---
+++ app/src/main/java/com/findingbetteryou/faby/modules/Dashboard.java
@@ -0,0 +1,16 @@
+package com.findingbetteryou.faby.modules;
+
+import android.os.Bundle;
+
+import com.findingbetteryou.faby.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
