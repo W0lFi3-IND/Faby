a7497b24c3cf1b8d4944c6c4e85d45f2994e2611
Yashovardhan Shaktawat <yashovardhanshaktawat@gmail.com>
2020-10-24 10:05:20 +0530

    OnBoarding

--- app/src/main/java/com/findingbetteryou/faby/OnBoarding/SplashScreen.java
+++ app/src/main/java/com/findingbetteryou/faby/OnBoarding/SplashScreen.java
@@ -1,8 +1,12 @@
 package com.findingbetteryou.faby.OnBoarding;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.findingbetteryou.faby.modules.MainActivity;
 import com.findingbetteryou.faby.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +16,18 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (FirebaseAuth.getInstance().getCurrentUser() != null){
+                    startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                    finish();
+                }
+                else {
+                    startActivity(new Intent(SplashScreen.this, PhoneVerification.class));
+                    finish();
+                }
+            }
+        },1700);
     }
-}
+    }
--- app/src/main/java/com/findingbetteryou/faby/modules/MainActivity.java
+++ app/src/main/java/com/findingbetteryou/faby/modules/MainActivity.java
@@ -1,8 +1,10 @@
-package com.findingbetteryou.faby;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.findingbetteryou.faby.modules;
 
 import android.os.Bundle;
+
+import com.findingbetteryou.faby.R;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
