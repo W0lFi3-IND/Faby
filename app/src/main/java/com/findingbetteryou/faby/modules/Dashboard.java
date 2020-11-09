1c97ef9f65c4f3c09082929e87f50e03103acedf
Yashovardhan Shaktawat <yashovardhanshaktawat@gmail.com>
2020-11-09 10:21:41 +0530

    Dashboard get data completed

--- app/src/main/java/com/findingbetteryou/faby/modules/Dashboard.java
+++ app/src/main/java/com/findingbetteryou/faby/modules/Dashboard.java
@@ -1,8 +1,10 @@
 package com.findingbetteryou.faby.modules;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.findingbetteryou.faby.R;
 import com.findingbetteryou.faby.caloriecal.CalorieDashboard;
@@ -11,12 +13,22 @@ import com.findingbetteryou.faby.waterlevel.WaterLevelDashboard;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Dashboard extends AppCompatActivity {
+TextView h,w,a,n;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        h = findViewById(R.id.Displayheight);
+        w = findViewById(R.id.DisplayWeight);
+        a = findViewById(R.id.DisplayAge);
+        n = findViewById(R.id.displayName);
 
+        SharedPreferences data = getSharedPreferences("data",0);
+        h.setText("Height : "+data.getString("Height","")+" Cm");
+        w.setText("Weight : "+data.getString("Weight","")+" Kg");
+        a.setText("Age : "+data.getString("Age",""));
+        n.setText(data.getString("Name",""));
         findViewById(R.id.CalorieBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
