794dfcae53ff7c4e65f1e73c243f29081231786e
Yashovardhan Shaktawat <yashovardhanshaktawat@gmail.com>
2020-11-16 19:33:45 +0530

    Changes to EnergyLevel

--- app/src/main/java/com/findingbetteryou/faby/MoodTracker/EnergyLevel.java
+++ app/src/main/java/com/findingbetteryou/faby/MoodTracker/EnergyLevel.java
@@ -1,7 +1,5 @@
 package com.findingbetteryou.faby.MoodTracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -9,8 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.findingbetteryou.faby.R;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class EnergyLevel extends AppCompatActivity {
 
@@ -18,6 +19,7 @@ public class EnergyLevel extends AppCompatActivity {
     private SeekBar seekBar;
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
+    private TextView Prog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +28,16 @@ public class EnergyLevel extends AppCompatActivity {
         sharedPreferences = getSharedPreferences("com.findingbetteryou.faby.MoodTracker", Context.MODE_PRIVATE);
         editor = sharedPreferences.edit();
 
-
         setContentView(R.layout.activity_energy_level);
         proceed = findViewById(R.id.buttonProceedEnergyLevel);
         seekBar = findViewById(R.id.seekBar);
-
+Prog = findViewById(R.id.progressNumber);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 editor.putInt("ENERGY_LEVEL", progress);
                 editor.commit();
+                Prog.setText(""+progress);
             }
 
             @Override
