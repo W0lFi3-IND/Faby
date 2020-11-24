5d7729a0e3c889ec8a0144ec3c576e20c2eea58d
mehakgagneja02 <55054817+mehakgagneja02@users.noreply.github.com>
2020-11-24 15:54:46 +0530

    Calories stored

---
+++ app/src/main/java/com/findingbetteryou/faby/caloriecal/CalDetails.java
@@ -0,0 +1,27 @@
+package com.findingbetteryou.faby.caloriecal;
+
+public class CalDetails {
+    public String item;
+    public String totalcal;
+
+    public CalDetails(String item, String totalcal) {
+        this.item = item;
+        this.totalcal = totalcal;
+    }
+
+    public CalDetails() {
+
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public String getTotalcal() {
+        return totalcal;
+    }
+
+
+}
+
+
---
+++ app/src/main/java/com/findingbetteryou/faby/caloriecal/Calhistory.java
@@ -0,0 +1,16 @@
+package com.findingbetteryou.faby.caloriecal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.findingbetteryou.faby.R;
+
+public class Calhistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calhistory);
+    }
+}
